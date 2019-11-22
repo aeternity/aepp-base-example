21873c6baf667a4c1e83b9d54d62d5f86d958887
Denis Davidyuk <denis_davidyuk@hotmail.com>
2019-11-22 08:16:14 +0100

    Improve UrlSchemeRpcClient

--- src/components/UrlSchemeRpcClient.js
+++ src/components/UrlSchemeRpcClient.js
@@ -1,81 +1,75 @@
+/* global localStorage */
+
 import stampit from '@stamp/it'
 import * as R from 'ramda'
 
-function addMethodToQueue(method) {
-  return function (...params) {
-    if (method === 'address') {
-      let address = localStorage.getItem('address')
-      return address
-        ? Promise.resolve(address)
-        : this.addMethodToQueue(method, params)
-    }
+let cachedAddress
 
-    return this.addMethodToQueue(method, params)
-  }
+const genProxyMethod = method => function (...params) {
+  if (method === 'address' && cachedAddress) return Promise.resolve(cachedAddress)
+  return this.addRequestToQueue(method, params)
 }
 
-const UrlSchemeRpcClient = stampit({
-  init() {
+const localStorageKey = 'UrlSchemeRpcClientResponse'
+
+export default stampit({
+  init () {
     const url = new URL(window.location.href)
     const result = url.searchParams.get('result')
+    const error = url.searchParams.get('error')
 
-    if (result) {
-      localStorage.setItem('result', decodeURIComponent(result))
+    if (result || error) {
+      localStorage.setItem(
+        localStorageKey,
+        JSON.stringify({
+          [error ? 'error' : 'result']: JSON.parse(decodeURIComponent(error || result))
+        })
+      )
       window.close()
-    } else {
-      window.addEventListener('storage', () => {
-        if (localStorage.getItem('result')) {
-          receive(localStorage.getItem('result'))
-        }
-      });
-    }
-    const queue = []
-    let waitingForRunningMethod = false
-
-    function receive(data) {
-      const result = JSON.parse(data)
-      const method = queue.shift()
-
-      if (result !== 'denied') {
-        if (method.name === 'address') localStorage.setItem('address', result)
-        method.resolve(result)
-      } else {
-        method.reject(result)
-      }
-
-      localStorage.removeItem('result')
-      waitingForRunningMethod = false
-
-      if (queue.length) {
-        callMethod(queue[0].name, queue[0].params)
-      }
     }
 
-    const buildDeeplinkUrl = (method, params) => {
+    window.addEventListener('storage', () => {
+      const response = localStorage[localStorageKey]
+      if (response) {
+        handleResponse(JSON.parse(response))
+        localStorage.removeItem(localStorageKey)
+      }
+    })
+
+    const requestQueue = []
+    let waitingForResponse = false
+
+    const handleResponse = ({ result, error }) => {
+      const request = requestQueue.shift()
+      if (result && request.name === 'address') cachedAddress = result
+      if (result) request.resolve(result)
+      else request.reject(error)
+      waitingForResponse = false
+      callMethodFromQueue()
+    }
+
+    const buildRequestUrl = (method, params) => {
       const url = new URL(`aepp-base:${method}`)
       params.forEach((p, i) => url.searchParams.set(`param${i}`, JSON.stringify(p)))
       url.searchParams.set('callback', window.location.origin)
       return url
     }
 
-    const callMethod = (method, params) => {
-      if (!waitingForRunningMethod) {
-        waitingForRunningMethod = true
-        window.location.href = buildDeeplinkUrl(method, params)
+    const callMethodFromQueue = () => {
+      if (!waitingForResponse && requestQueue.length) {
+        const { name, params } = requestQueue[0]
+        waitingForResponse = true
+        window.location.href = buildRequestUrl(name, params)
       }
     }
 
-    this.addMethodToQueue = (method, params) => {
+    this.addRequestToQueue = (name, params) => {
       const p = new Promise((resolve, reject) => {
-        queue.push({ name: method, params, resolve, reject })
+        requestQueue.push({ name, params, resolve, reject })
       })
-
-      callMethod(method, params)
-
+      callMethodFromQueue()
       return p
     }
   },
-  methods: R.fromPairs(['address', 'sign', 'signTransaction'].map(m => [m, addMethodToQueue(m)])),
+  methods: R.fromPairs(['address', 'sign', 'signTransaction'].map(m => [m, genProxyMethod(m)]))
 })
-
-export default UrlSchemeRpcClient
